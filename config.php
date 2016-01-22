9d8dc453801366b71e1a2c9265f87b5906142822
ugly-yh <yinhao746746@163.com>
2016-01-22 12:11:43 +0800

    v0.1

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* 生成配置文件
+* 保存数据库连接以及用户信息
+*/
+
+/** 使用的数据库类型 */
+define('DB_TYPE', 'mysql');
+
+/** Gahter数据库的名称 */
+define('DB_NAME', 'gather');
+
+/** 数据库用户名 */
+define('DB_USER', 'root');
+
+/** 数据库密码 */
+define('DB_PASS', '123456');
+
+/** 主机 */
+define('DB_HOST', 'localhost');
+
+/** 创建数据表时默认的文字编码 */
+define('DB_CHARSET', 'utf8');
+
+
+/** 系统用户 */
+define('GA_USER', 'admin');
+
+/** 系统密码 */
+define('GA_PASS', 'admin');
+
+?>
